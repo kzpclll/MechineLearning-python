150272f786abcab8ddf768ed173ab86f519ad7f5
kzpclll <386789227@qq.com>
2021-12-21 17:12:52 +0800

    trees -12/21

---
+++ readme.txt
@@ -0,0 +1,6 @@
+使用时进入pycharm使用普通命令行
+python命令进入python
+import trees
+import importlib
+importlib.reload(trees)
+
---
+++ trees.py
@@ -0,0 +1,60 @@
+from numpy import *
+import operator
+from math import log
+
+
+# 计算香农熵
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        prob = float(labelCounts[key]) / numEntries
+        shannonEnt -= prob * math.log(prob, 2)
+    return shannonEnt
+
+
+# 创建数据集
+def createDataSet():
+    dataset = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataset, labels
+
+
+# 划分数据集
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            # 取到下标为axis的特征进行比对，并取出下标前的数据、下标后的数据extend之后作为一个列表append到dataSet里面
+            reducedFeatVec = featVec[:axis]
+            # extend 合并列表元素 append 将列表作为一个新元素插入
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+# 遍历最好数据划分
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVials = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVials:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = 1
+    return bestFeature
