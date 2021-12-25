17b7618702f24f83563883e2f9f7d75353d1e1b6
kzpclll <386789227@qq.com>
2021-12-25 16:27:14 +0800

    finish decisionTree congratulations

--- trees.py
+++ trees.py
@@ -1,11 +1,13 @@
+from copy import deepcopy
+
 from numpy import *
 import numpy as np
 import operator
 import csv
 import random
+import math
 
 
-# 数据处理与公用接口 <--                       -->
 # 计算香农熵
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
@@ -70,12 +72,53 @@ def dataDispersed(dataSet):
 
 
 # 使用留出法将原数据集划分为 8:2 的数据集合测试集
-def dataSplitToTestData(dataSet):
-    length = int(len(dataSet) / 5)
-    testSet = []
-    for i in range(length):
-        testSet.append(dataSet.pop(random.randint(0, len(dataSet) - 1)))
-    return dataSet, testSet
+# 修改留出法，没有遵循分层采样、互斥采样的原则，所以做重新修改
+# 将数据集分层拆分为5个独立的集，并用于计算、评估
+# important
+def dataSplitToFive(dataSet):
+    # 获取所有的label值
+    featList = [rec[-1] for rec in dataSet]
+    # 创建无重复label集合
+    uniqueVials = set(featList)
+    originSet = [[], [], [], [], [], [], [], [], [], []]
+
+    # 一个看起来就非常慢的双循环将所有数据按label分到自己的类别中
+    # 拿一个numRec记录每个分类有多少个
+    for label1 in uniqueVials:
+        label1 = int(label1)
+        for rec1 in dataSet:
+            if rec1[-1] == label1:
+                originSet[label1].append(rec1)
+
+    # 做完了输出一哈结果
+    print("收集到的label如下：")
+    print(uniqueVials)
+    print("每个label的数量统计如下：")
+    for rec1 in originSet:
+        print(len(rec1))
+
+    count = np.zeros((10, 1))
+    splitSets = [[], [], [], [], []]
+    for labelName in uniqueVials:
+        labelName = int(labelName)
+        count[labelName] = math.ceil(len(originSet[labelName]) / 5)
+
+    print(count)
+    # 有五个数据集，每个数据集对原集合中所有的label分别采样指定次数，并解决无法整除的问题
+    for n in range(5):
+        for labelName in uniqueVials:
+            labelName = int(labelName)
+            num = deepcopy(count[labelName])
+            while originSet[labelName] and num > 0:
+                temp = (originSet[labelName].pop(random.randint(0, len(originSet[labelName]) - 1))).tolist()
+                splitSets[n].append(temp)
+                num -= 1
+
+    # 输出划分结果：
+    print("划分数据集完成！")
+    for n in range(5):
+        print(len(splitSets[n]))
+    return splitSets
 
 
 # 创建数据集
@@ -85,7 +128,7 @@ def createDataSet():
     return dataset, labels
 
 
-# 划分数据集 将数据集中的下标为axis的数据剔除
+# 划分数据集
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
@@ -104,7 +147,7 @@ def majorityCnt(classList):
     for vote in classList:
         if vote not in classCount.keys(): classCount[vote] = 0
         classCount[vote] += 1
-    sortedClassCount = sorted(classCount.iteritemsd(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 
@@ -117,13 +160,13 @@ def chooseBestFeatureToSplit(dataSet):
     bestFeature = -1
     for i in range(numFeatures):
         # 获取目标特征的所有属性
-        featList = [example[i] for example in dataSet]
+        featList2 = [example[i] for example in dataSet]
         # 创建无重复属性集合
-        uniqueVials = set(featList)
+        uniqueVials1 = set(featList2)
         newEntropy = 0.0
         # 遍历每个属性，计算单个属性划分之后的信息增益大小
         # 累加所有属性的加权信息增益，计算增益率
-        for value in uniqueVials:
+        for value in uniqueVials1:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet) / float(len(dataSet))
             newEntropy += prob * calcShannonEnt(subDataSet)
@@ -137,8 +180,9 @@ def chooseBestFeatureToSplit(dataSet):
 
 # 递归构造dict形式的决策树 dataSet是有label的list，featNames是记录特征名称的list
 # important
-def createTree(dataSet, featNames):
+def createTree(dataSet, featName):
     # 判断是否达到递归的特殊条件
+    featNames = featName[:]
     classList = [example[-1] for example in dataSet]
     if classList.count(classList[0]) == len(classList):
         return classList[0]
@@ -161,8 +205,8 @@ def createTree(dataSet, featNames):
 
 
 # 使用决策树对测试数据集进行分类
-# ready to do : understand this ->
-def classify(inputTree, featLabels, testRec):
+def classify(inputTree, featLabel, testRec):
+    featLabels = featLabel[:]
     # 获取当前字典的特征名
     feat = list(inputTree.keys())[0]
     # 使用这个特征名从决策树中获取该特征分类下的所有属性分类字典
@@ -179,34 +223,41 @@ def classify(inputTree, featLabels, testRec):
                 classLabel = classify(nextDict[featValue], featLabels, testRec)
             else:
                 classLabel = nextDict[featValue]
-                print("rec:")
-                print(classLabel)
+                # print("rec:")
+                # print(classLabel)
             return classLabel
 
 
 # 对批量数据进行分类
-def batchClassify(inputTree, featLabels, testSet):
-    for rec in testSet:
-        classify(inputTree, featLabels, rec)
+def batchClassifyAndEstimate(inputTree, featLabels, testSet1):
+    total = len(testSet1)
+    count = 0
+    for rec1 in testSet1:
+        ansLabel = classify(inputTree, featLabels, rec1)
+        if ansLabel == rec1[-1]:
+            count += 1
+    correctRating = (count / total) * 100
+    print("使用 2 8 留出法测试正确率")
+    print(correctRating, "%")
+    return correctRating
 
 
-
-
-with open("winequality-red.csv") as wineInfo:
+with open("bill_authentication.csv") as wineInfo:
     csvReader = csv.reader(wineInfo)
     wineInfoList = list(csvReader)
+    # 拆解数据 和 标签名
     data, label = dataDispersed(wineInfoList)
+    splitDatas = dataSplitToFive(data)
+    # print(splitData)
     # data, label = createDataSet()
     data = data.tolist()
-    data, testData = dataSplitToTestData(data)
-    labelCreate = label[:]
-    tree = createTree(data, labelCreate)
 
-    print("dict tree:")
-    print(tree)
-    # print("label:")
-    # print(label)
-    rec = [3.0, 11.0, 1.0, 1.0, 3.0, 1.0, 1.0, 4.0, 6.0, 1.0, 4.0, 3.0]
-    classify(tree, label, rec)
+    for n in range(5):
+        splitData = splitDatas[:]
+        testSet = splitData.pop(n)
+        createSet = []
+        for set1 in splitData:
+            createSet.extend(set1)
+        tree = createTree(createSet, label)
+        batchClassifyAndEstimate(tree, label, testSet)
 
-    # batchClassify(tree, label, testData)
